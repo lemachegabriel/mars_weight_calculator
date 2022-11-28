e337ec0823bacbaabc198d418112ecf100c7d1bf
lemachegabriel <lemache35@gmail.com>
2022-11-27 21:34:27 -0300

    mars weight calculator

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+fn main() {
+  println!("Enter your weight:");
+  let mut input = String::new();
+  io::stdin().read_line(&mut input).unwrap();
+
+  let weight: f32 = input.trim().parse().unwrap(); 
+
+  let mars_weight = calculate_weight_on_mars(weight);
+  println!("Weight on mars {}", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+  (weight / 9.81) * 3.711 
+}
